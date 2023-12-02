4f000e0f6ca058c036ef82284a010ee5739b8c98
ijazch <chijaz429@gmail.com>
2023-12-02 16:03:58 +0000

    ok

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 window.addEventListener("keydown", function (e) {
+  const audio = this.document.querySelector(`audio[data-key="${e.keycode}"]`);
+  if (!audio) return; // nothing will be happen
+  audio.currentTime = 0;
+  audio.play();
   console.log(e);
 });
