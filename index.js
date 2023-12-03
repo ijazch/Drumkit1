6abbd3cc2912065ceb4047f6b96be1152fb838c9
ijazch <chijaz429@gmail.com>
2023-12-03 15:13:53 +0000

    c

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.addEventListener("keydown", function (e) {
   const audio = this.document.querySelector(`audio[data-key="${e.keycode}"]`);
+  const key = this.document.querySelector(`.key[data-key="${e.keycode}"]`);
   if (!audio) return; // nothing will be happen
   audio.currentTime = 0; // come back
   audio.play();
